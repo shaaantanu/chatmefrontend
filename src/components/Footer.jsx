6e0fd169a62657b9f126544254173d4d5d4249f9
shaantanu <shantanus2410@gmail.com>
2023-12-13 20:44:41 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Footer() {
+  return (
+    <div><footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div className="col-md-4 d-flex align-items-center">
+      <span className="text-muted" style={{color:'white', fontSize: '17px'}}>© 2023 ChatMe (Created by Shantanu Sahay)</span>
+    </div>
+  </footer></div>
+  )
+}
