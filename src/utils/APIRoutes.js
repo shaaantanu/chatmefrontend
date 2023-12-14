4017f9604e3cb0e3127ae57ab94b559a5ddd0107
shaaantanu <87699595+shaaantanu@users.noreply.github.com>
2023-12-14 22:45:58 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://backendchat-dgh5.onrender.com";
+export const host = "https://chatmebackend-hssi.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
